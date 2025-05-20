4da51d4fff5e60089d105f152ca38066e9c4d028
duvanrizky <duvanrizky07@gmail.com>
2025-05-21 02:42:40 +0700

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000', // move baseUrl here
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here if needed
     },
   },
 });
--- cypress/e2e/1-getting-started/Login.cy.js
+++ cypress/e2e/1-getting-started/Login.cy.js
@@ -11,27 +11,3 @@ describe('pom', () =>{
     });
 
 });
-
-//     export default PageObject
-
-// //     it.block('login with invalid credential', () => {
-// //         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-// //         cy.get('input[name="username"]').type('admin')
-// //         cy.get('input[name="password"]').type('admin12') 
-// //         cy.get('.oxd-button').click()
-// //     });
-
-// //     it.block('login with username credential', () => {
-// //         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-// //         cy.get('input[name="username"]').type('adminabcd')
-// //         cy.get('input[name="password"]').type('admin123') 
-// //         cy.get('.oxd-button').click()
-// //     });
-
-// //     it.block('login with password credential', () => {
-// //         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-// //         cy.get('input[name="username"]').type('admin')
-// //         cy.get('input[name="password"]').type('admin123abcd') 
-// //         cy.get('.oxd-button').click()
-// //     });
-// });
